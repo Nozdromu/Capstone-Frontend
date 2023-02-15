8fc6f1a947d5538af0984b39305318039f344601
Nozdromu <yileiding1990@gmail.com>
2023-02-15 12:24:30 -0800

    same bug fix

--- Client/src/Components/Newchatwindow.js
+++ Client/src/Components/Newchatwindow.js
@@ -3,13 +3,13 @@ import { useState, useEffect } from 'react';
 import Core from './Core';
 
 export default function Chatwindow(props) {
-    var h=[...Core.getrooms().getroom(props.roomid).gethistory()].reverse()
+    var h = [...Core.getrooms().getroom(props.roomid).gethistory()].reverse()
     const [history, sethistory] = useState(h);
     const [count, setcount] = useState(0);
     var roomid = props.roomid
     var room = Core.getrooms().getroom(roomid);
     var set = () => {
-        h=[...Core.getrooms().getroom(props.roomid).gethistory()].reverse()
+        h = [...Core.getrooms().getroom(props.roomid).gethistory()].reverse()
         sethistory(history => h);
         setcount(count => count + 1)
     }
@@ -18,11 +18,13 @@ export default function Chatwindow(props) {
         room.setupdatehistory(set);
     }, [])
 
-    return (<Container style={{ maxHeight: '100%', 
-    overflowY: 'auto',
-    display: 'flex',
-    'flex-direction': 'column-reverse' }}>
+    return (<Container style={{
+        maxHeight: '100%',
+        overflowY: 'auto',
+        display: 'flex',
+        flexDirection: 'column-reverse'
+    }}>
         <div style={{ 'textAlign': 'center' }}>{props.roomid}</div>
-        {history.reverse()}
+        {history}
     </Container>)
 }
