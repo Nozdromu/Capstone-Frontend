90715034d7c4931026d8f5623b9c105921b5616a
Nozdromu <yileiding1990@gmail.com>
2023-02-20 15:48:01 -0800

    delete unuse folder

--- backend/users/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import AppUser
-
-
-class MainUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AppUser
-        fields = "__all__"
